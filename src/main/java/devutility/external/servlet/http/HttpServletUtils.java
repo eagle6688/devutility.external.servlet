6734478a442633ac5b50693ebe617ab1a49e1c0f
Aldwin Su <fangyansu@163.com>
2018-11-19 18:25:38 +0800

    Add HttpServletUtils.java

---
+++ src/main/java/devutility/external/servlet/http/HttpServletUtils.java
@@ -0,0 +1,34 @@
+package devutility.external.servlet.http;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpServletUtils {
+	/**
+	 * Response data to client through HttpServletResponse.
+	 * @param httpServletResponse: HttpServletResponse object.
+	 * @param data: String data.
+	 */
+	public static void response(HttpServletResponse httpServletResponse, String data) {
+		response(httpServletResponse, data, HttpServletResponse.SC_OK);
+	}
+
+	/**
+	 * Response data to client through HttpServletResponse.
+	 * @param httpServletResponse: HttpServletResponse object.
+	 * @param data: String data.
+	 * @param status: Http status.
+	 */
+	public static void response(HttpServletResponse httpServletResponse, String data, int status) {
+		httpServletResponse.setStatus(status);
+		httpServletResponse.setContentType("application/json; charset=utf-8");
+
+		try (ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream()) {
+			byte[] bytes = data.getBytes("UTF-8");
+			httpServletResponse.setContentLength(bytes.length);
+			servletOutputStream.write(bytes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
