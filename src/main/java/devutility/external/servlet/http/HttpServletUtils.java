89b0abf1142fe5e33e7b27246ed4371bfdf7478b
Aldwin Su <fangyansu@163.com>
2019-02-08 17:39:46 +0800

    Add unauthorizedResponse and forbiddenResponse

--- src/main/java/devutility/external/servlet/http/HttpServletUtils.java
+++ src/main/java/devutility/external/servlet/http/HttpServletUtils.java
@@ -6,27 +6,36 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HttpServletUtils {
 	/**
-	 * Response data to client through HttpServletResponse.
-	 * @param httpServletResponse HttpServletResponse object.
-	 * @param data: String data.
+	 * Get request url with parameters.
+	 * @param request HttpServletRequest object.
+	 * @return String
 	 */
-	public static void response(HttpServletResponse httpServletResponse, String data) {
-		response(httpServletResponse, data, HttpServletResponse.SC_OK);
+	public static String getRequestUrl(HttpServletRequest request) {
+		return String.format("%s?%s", request.getRequestURL().toString(), request.getQueryString());
 	}
 
 	/**
 	 * Response data to client through HttpServletResponse.
-	 * @param httpServletResponse HttpServletResponse object.
+	 * @param response HttpServletResponse object.
 	 * @param data: String data.
-	 * @param status: Http status.
 	 */
-	public static void response(HttpServletResponse httpServletResponse, String data, int status) {
-		httpServletResponse.setStatus(status);
-		httpServletResponse.setContentType("application/json; charset=utf-8");
+	public static void response(HttpServletResponse response, String data) {
+		response(response, data, HttpServletResponse.SC_OK);
+	}
 
-		try (ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream()) {
+	/**
+	 * Response data to client through HttpServletResponse.
+	 * @param response HttpServletResponse object.
+	 * @param data Serialized response data.
+	 * @param status Http status.
+	 */
+	public static void response(HttpServletResponse response, String data, int status) {
+		response.setStatus(status);
+		response.setContentType("application/json; charset=utf-8");
+
+		try (ServletOutputStream servletOutputStream = response.getOutputStream()) {
 			byte[] bytes = data.getBytes("UTF-8");
-			httpServletResponse.setContentLength(bytes.length);
+			response.setContentLength(bytes.length);
 			servletOutputStream.write(bytes);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -34,11 +43,20 @@ public class HttpServletUtils {
 	}
 
 	/**
-	 * Get request url with parameters.
-	 * @param request HttpServletRequest object.
-	 * @return String
+	 * Unauthorized response.
+	 * @param response HttpServletResponse object.
+	 * @param data Serialized response data.
 	 */
-	public static String getRequestUrl(HttpServletRequest request) {
-		return String.format("%s?%s", request.getRequestURL().toString(), request.getQueryString());
+	public static void unauthorizedResponse(HttpServletResponse response, String data) {
+		response(response, data, HttpServletResponse.SC_UNAUTHORIZED);
+	}
+
+	/**
+	 * Forbidden response.
+	 * @param response HttpServletResponse object.
+	 * @param data Serialized response data.
+	 */
+	public static void forbiddenResponse(HttpServletResponse response, String data) {
+		response(response, data, HttpServletResponse.SC_FORBIDDEN);
 	}
 }
